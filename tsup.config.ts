379e816ab92b645df10b53e0f2cfcb28f0ad0d5d
hanzydev <emirhanbaltas34@gmail.com>
2023-09-18 21:55:19 +0300

    feat: prepare for publish

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     target: "esnext",
-    dts: false,
+    dts: {
+        resolve: true,
+        entry: './src/index.ts',
+    },
     keepNames: true,
     entryPoints: ['./src/**/*.ts'],
     clean: true,
