f06493f07416d24f80c935a727aa61fe8e1a1431
hanzydev <emirhanbaltas34@gmail.com>
2023-12-23 01:11:47 +0300

    DAMNNNN

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,96 @@ export * from './structures/Hook';
 export * from './types';
 
 // export fastify types
-export type * from 'fastify/types/instance';
-export type * from 'fastify/types/hooks';
-export type * from 'fastify/types/logger';
-export type * from 'fastify/types/request';
-export type * from 'fastify/types/reply';
-export type * from 'fastify/types/errors';
-export type * from 'fastify/types/register';
-export type * from 'fastify/types/plugin';
-export type * from 'fastify/types/route';
-export type * from 'fastify/types/schema';
-export type * from 'fastify/types/context';
-export type * from 'fastify/types/utils';
-export type * from 'fastify/types/serverFactory';
-export type * from 'fastify/types/type-provider';
-export type * from 'fastify/types/content-type-parser';
+export type {
+    FastifyBodyParser,
+    FastifyContentTypeParser,
+    AddContentTypeParser,
+    hasContentTypeParser,
+    getDefaultJsonParser,
+    ProtoAction,
+    ConstructorAction,
+} from 'fastify/types/content-type-parser';
+export type { FastifyContext, FastifyContextConfig } from 'fastify/types/context';
+export type { FastifyErrorCodes } from 'fastify/types/errors';
+export type {
+    DoneFuncWithErrOrRes,
+    HookHandlerDoneFunction,
+    RequestPayload,
+    onCloseAsyncHookHandler,
+    onCloseHookHandler,
+    onErrorAsyncHookHandler,
+    onErrorHookHandler,
+    onReadyAsyncHookHandler,
+    onReadyHookHandler,
+    onListenAsyncHookHandler,
+    onListenHookHandler,
+    onRegisterHookHandler,
+    onRequestAsyncHookHandler,
+    onRequestHookHandler,
+    onResponseAsyncHookHandler,
+    onResponseHookHandler,
+    onRouteHookHandler,
+    onSendAsyncHookHandler,
+    onSendHookHandler,
+    onTimeoutAsyncHookHandler,
+    onTimeoutHookHandler,
+    preHandlerAsyncHookHandler,
+    preHandlerHookHandler,
+    preParsingAsyncHookHandler,
+    preParsingHookHandler,
+    preSerializationAsyncHookHandler,
+    preSerializationHookHandler,
+    preValidationAsyncHookHandler,
+    preValidationHookHandler,
+    onRequestAbortHookHandler,
+    onRequestAbortAsyncHookHandler,
+} from 'fastify/types/hooks';
+export type {
+    FastifyListenOptions,
+    FastifyInstance,
+    PrintRoutesOptions,
+} from 'fastify/types/instance';
+export type {
+    FastifyBaseLogger,
+    FastifyLoggerInstance,
+    FastifyLoggerOptions,
+    PinoLoggerOptions,
+    FastifyLogFn,
+    LogLevel,
+    Bindings,
+    ChildLoggerOptions,
+} from 'fastify/types/logger';
+export type {
+    FastifyPluginCallback,
+    FastifyPluginAsync,
+    FastifyPluginOptions,
+    FastifyPlugin,
+} from 'fastify/types/plugin';
+export type {
+    FastifyRegister,
+    FastifyRegisterOptions,
+    RegisterOptions,
+} from 'fastify/types/register';
+export type { FastifyReply } from 'fastify/types/reply';
+export type { FastifyRequest, RequestGenericInterface } from 'fastify/types/request';
+export type {
+    RouteHandler,
+    RouteHandlerMethod,
+    RouteOptions,
+    RouteShorthandMethod,
+    RouteShorthandOptions,
+    RouteShorthandOptionsWithHandler,
+    RouteGenericInterface,
+} from 'fastify/types/route';
+export type {
+    FastifySchema,
+    FastifySchemaCompiler,
+    FastifySchemaValidationError,
+    SchemaErrorDataVar,
+    SchemaErrorFormatter,
+} from 'fastify/types/schema';
+export type {
+    FastifyServerFactory,
+    FastifyServerFactoryHandler,
+} from 'fastify/types/serverFactory';
+export type { FastifyTypeProvider, FastifyTypeProviderDefault } from 'fastify/types/type-provider';
